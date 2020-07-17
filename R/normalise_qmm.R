22fcd31bc924fbe9e8fb4eda64bb8b88064a15fd
Rafael Sommer <rsommer@l3949914.portoalegre.pucrsnet.br>
2020-07-17 18:16:34 -0300

    normalise_qmm

---
+++ R/normalise_qmm.R
@@ -0,0 +1,22 @@
+#' Caculate the Normalised Leptokurtic Diffusion from a q-Space Myelin Map
+#' @importFrom neurobase readnii writenii
+#' @param qmm Nifti object of a processed myelinmap
+#' @param outfile Output file name
+#' @param write If = TRUE, write a Nifti file for the NLD in the current directory
+#' @param verbose If = TRUE show log messages
+#' @return A calculated NLD nifti object
+#' @author R.C.S
+#' @export
+
+normalise_qmm <- function (qmm, outfile = NULL, write = TRUE, verbose = TRUE) {
+  if(verbose){message("Calculating NLD")}
+  nld <- ((max(qmm) - qmm) / (max(qmm)-min(qmm[qmm>0])) ) * 100
+  mask = nld < 1000
+  if(verbose){message("Masking")}
+  nld = nld * mask
+  if (write){
+    if(verbose){message("Writing Nifti")}
+    writenii(nld, outfile)
+    }
+    return(nld)
+  }
